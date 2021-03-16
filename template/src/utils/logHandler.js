9e4cbc425150772a64b8369c386000eb43abeca6
Eknut Wataorndilok <mean.dp589@gmail.com>
2021-03-16 14:18:47 +0700

    ignore hralth-check

--- template/src/utils/logHandler.js
+++ template/src/utils/logHandler.js
@@ -60,7 +60,12 @@ module.exports = {
                 msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
                 expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
                 colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-                ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+                ignoreRoute: function (req, res) { 
+                    if(req.path == '/health-check') {
+                        return true
+                    }
+                    return false
+                } // optional: allows to skip some log messages based on request and/or response
             }));
         }
     }
